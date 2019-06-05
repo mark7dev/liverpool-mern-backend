a219a536388fa984c64e3d641c32c0532a39ab1b
mark7dev <mark7dev@hotmail.com>
2019-06-05 16:18:04 -0500

    increase limit urlencoded

--- app.js
+++ app.js
@@ -26,8 +26,9 @@ ODM.connection.on('connected', () => {
 
 app.use(logger('dev'));
 app.use('/static', express.static('public'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({
+    limit: '50mb',
     extended: true
 }));
 
